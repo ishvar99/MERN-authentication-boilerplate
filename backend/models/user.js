27db91dfe75d40cc0b3eac58caaf8a0da4096059
ishvar99 <ishanvarshney99@gmail.com>
2020-09-09 13:07:17 +0530

    added authentication

---
+++ backend/models/user.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const crypto = require("crypto")
+const {
+  data: { JWT_SECRET, JWT_EXPIRE },
+} = require("../config/keys")
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      min: 3,
+      required: [true, "Please provide a name"],
+    },
+    age: {
+      type: Number,
+      trim: true,
+      required: [true, "Please provide an age"],
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email",
+      ],
+      required: [true, "Please provide an email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: [6, "Password should be atleast 6 characters"],
+      select: false, //never returns password when we call user
+    },
+    role: {
+      type: Number,
+      default: 0,
+      // 0 - simple user
+      // 2 - admin / superuser
+    },
+    confirmed: {
+      type: Boolean,
+      default: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+)
+userSchema.pre("save", async function (next) {
+  const user = this
+  if (!user.isModified("password")) {
+    next()
+  }
+  const salt = await bcrypt.genSalt(10)
+  user.password = await bcrypt.hash(user.password, salt)
+  next()
+})
+userSchema.methods.getSignedJwtToken = function () {
+  const user = this
+  return jwt.sign({ id: user._id }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRE,
+  })
+}
+userSchema.methods.comparePasswords = async function (plainPassword) {
+  const user = this
+  return await bcrypt.compare(plainPassword, user.password)
+}
+userSchema.methods.getResetPasswordToken = function () {
+  const user = this
+  const resetToken = crypto.randomBytes(20).toString("hex")
+  user.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex")
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000
+  return resetToken
+}
+
+module.exports = mongoose.model("User", userSchema)
---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken")
+const sgMail = require("@sendgrid/mail")
+const {
+  data: { SENDER_EMAIL },
+} = require("../config/keys")
+exports.sendEmail = async (url, user, type) => {
+  let template_data = {}
+  if (type === "confirm_account") {
+    template_data = {
+      name: user.name,
+      confirm_account: url,
+    }
+  } else if (type === "reset_password") {
+    template_data = {
+      email: user.email,
+      reset_password: url,
+    }
+  }
+  const msg = {
+    to: user.email,
+    from: SENDER_EMAIL,
+    templateId: templates[type],
+    dynamic_template_data: template_data,
+  }
+  await sgMail.send(msg)
+  console.log(`Account confirmation mail sent to ${user.email}`.cyan.bold)
+}
