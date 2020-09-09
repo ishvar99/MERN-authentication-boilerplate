d8c6b23fafa6e7a02ef94ad91dd53e98d1694425
Ishan Varshney <44304220+ishvar99@users.noreply.github.com>
2020-09-09 16:25:55 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-      <h1>ECOMMERCE WEBSITE</h1>
+      <h1>ECOMMERCE WEBSITE WITH ML AND BLOCKCHAIN</h1>
     </div>
   )
 }
