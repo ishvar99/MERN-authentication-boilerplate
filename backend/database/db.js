41fb910c5158fe1491cb9c62d9baac521d277bb9
ishvar99 <ishanvarshney99@gmail.com>
2020-09-15 23:48:37 +0530

    changed mongo environment to production

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose")
-const {
-  data: { mongoDevelopmentURI },
-} = require("../config/keys")
+// const {
+//   data: { mongoDevelopmentURI },
+// } = require("../config/keys")
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(mongoDevelopmentURI, {
+    const conn = await mongoose.connect(process.env.mongoProductionURI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: true,
