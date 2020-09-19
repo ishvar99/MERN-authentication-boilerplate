2195731142e0919f831badcd65bf665f2107593f
ishvar99 <ishanvarshney99@gmail.com>
2020-09-19 21:03:28 +0530

    showing account confirmation reminder

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,8 +1,24 @@
 import React from "react"
 // import PropTypes from "prop-types"
-
+import { useSelector, useDispatch } from "react-redux"
 const Home = (props) => {
-  return <div>Home</div>
+  const auth = useSelector((state) => state.auth)
+  const { user } = auth
+  return (
+    <>
+      {user && !user.confirmed && (
+        <div
+          style={{ textAlign: "center" }}
+          class="alert alert-warning"
+          role="alert"
+        >
+          Confirmation mail send to{" "}
+          <a href={"mailto:" + user.email}>{user.email}</a>. Please confirm your
+          account to get started.
+        </div>
+      )}
+    </>
+  )
 }
 
 // Home.propTypes = {}
